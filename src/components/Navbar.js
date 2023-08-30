8742ce9e2f5fef7f72db4afad95e297462af6a6b
George-k-tech <kariukigeorge511@gmail.com>
2023-08-30 17:47:23 +0300

    changes in the home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,30 @@ import "../main.css";
 const Navbar = () => {
   return (
     <div className="navbar">
-
       <div className="navbar-logo">
-        <NavLink to="/">George Kariuki</NavLink>
+        <NavLink to="/" className="a">George Kariuki</NavLink>
       </div>
 
-      <ul className="navlinks">
-      <input type="checkbox" id="checkbox-toggle"></input>
-      <label htmlFor="checkbox-toggle" className="hamburger">&#9776;</label>
-        <div className="menu">
-        <li><NavLink to="/work" className="a">Recent Work</NavLink></li>
-        <li><NavLink to="/contact" className="a">Contacts</NavLink></li>
-        </div>
-      </ul>
+      <div className="navbar-body">
+        <ul className="navlinks">
+          <input type="checkbox" id="checkbox-toggle"></input>
+          <label htmlFor="checkbox-toggle" className="hamburger">
+            &#9776;
+          </label>
+          <div className="menu">
+            <li>
+              <NavLink to="/work" className="a">
+                Recent Work
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacts" className="a">
+                Contacts
+              </NavLink>
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,24 @@
 const Home = () => {
   return (
     <section>
-      <div className="home">
-        <div className="home-left">
-          <h2>Full-Stack Developer </h2>
-
-          <div className="home-left-head">
-            <div className="home-left-head-desc">
-              <h3>My Stacks:</h3>
-            </div>
-
-            <div className="home-left-head-gif">
-
-              <div className="home-left-head-gif-gif" id="gif1">
-                <h4>Laravel</h4>
-              </div>
-
-              <div className="home-left-head-gif-gif" id="gif2">
-                <h4>React</h4>
-              </div>
-
-              <div className="home-left-head-gif-gif" id="gif3">
-                <h4>Flutter</h4>
-              </div>
-
-              <div className="home-left-head-gif-gif" id="gif4">
-                <h4>Angular</h4>
-              </div>
-
-            </div>
-          </div>
-
-          <div className="home-left-body"></div>
+      <div className="header">
+        <div class="context">
+          <p>Hello </p>
         </div>
 
-        <div className="home-right">
-          <p>Image</p>
+        <div class="area">
+          <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
       </div>
     </section>
