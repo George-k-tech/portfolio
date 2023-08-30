f4072c7d86cf3e6aa304269ff28e4c93ae47c34b
George-k-tech <kariukigeorge511@gmail.com>
2023-08-30 14:08:50 +0300

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,18 @@ import "../main.css";
 const Navbar = () => {
   return (
     <div className="navbar">
+
       <div className="navbar-logo">
         <NavLink to="/">George Kariuki</NavLink>
       </div>
-      <ul>
-        <li><NavLink to="/work">Recent Work</NavLink></li>
-        <li><NavLink to="/contact">Contacts</NavLink></li>
+
+      <ul className="navlinks">
+      <input type="checkbox" id="checkbox-toggle"></input>
+      <label htmlFor="checkbox-toggle" className="hamburger">&#9776;</label>
+        <div className="menu">
+        <li><NavLink to="/work" className="a">Recent Work</NavLink></li>
+        <li><NavLink to="/contact" className="a">Contacts</NavLink></li>
+        </div>
       </ul>
     </div>
   );
