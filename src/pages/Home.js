7491a3ebf2d94e6a342f0fa7882ec0645c37bff9
George-k-tech <kariukigeorge511@gmail.com>
2023-08-31 18:30:00 +0300

    about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,21 +2,21 @@ const Home = () => {
   return (
     <section>
       <div className="header">
-          <div class="context">
-            <div class="reveal-container">
-              <div class="reveal-text">
-                <p className="p">
-                  Hello, <span class="wave">👋</span>
-                </p>
-              </div>
-            </div>
-            <div>
+        <div class="context">
+          <div class="reveal-container">
+            <div class="reveal-text">
               <p className="p">
-                My Name is <span className="name">George Mungai Kariuki,</span>
+                Hello, <span class="wave">👋</span>
               </p>
-              <p className="p">I'm a Fullstack Developer.</p>
             </div>
           </div>
+          <div>
+            <p className="p">
+              My Name is <span className="name">George Mungai Kariuki,</span>
+            </p>
+            <p className="p">I'm a Fullstack Developer.</p>
+          </div>
+        </div>
         <div class="area">
           <ul class="circles">
             <li></li>
@@ -30,6 +30,30 @@ const Home = () => {
             <li></li>
             <li></li>
           </ul>
+        </div>
+      </div>
+
+      <div className="about">
+
+        <div className="about-head">
+          <p>About</p>
+        </div>
+        <div className="about-icons">
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+              
+            </div>
         </div>
       </div>
     </section>
