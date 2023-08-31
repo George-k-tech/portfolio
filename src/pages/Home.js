1cf4b68876f482d25147dc7ac086d5d87e5cfd01
George-k-tech <kariukigeorge511@gmail.com>
2023-08-31 17:46:46 +0300

    box shadow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,26 +2,21 @@ const Home = () => {
   return (
     <section>
       <div className="header">
-        <div class="context">
-          <div class="reveal-container">
-            <div class="reveal-text">
+          <div class="context">
+            <div class="reveal-container">
+              <div class="reveal-text">
+                <p className="p">
+                  Hello, <span class="wave">👋</span>
+                </p>
+              </div>
+            </div>
+            <div>
               <p className="p">
-                Hello, <span class="wave">👋</span>
+                My Name is <span className="name">George Mungai Kariuki,</span>
               </p>
+              <p className="p">I'm a Fullstack Developer.</p>
             </div>
           </div>
-          <div>
-            <p className="p">
-              My Name is <span className="name">George Mungai Kariuki,</span>
-            </p>
-            <p className="p">I'm a Fullstack Developer.</p>
-          </div>
-
-          <div className="div-button">
-            
-          </div>
-        </div>
-
         <div class="area">
           <ul class="circles">
             <li></li>
